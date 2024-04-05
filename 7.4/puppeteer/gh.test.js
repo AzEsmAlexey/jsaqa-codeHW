f5563fb3f7623359f5751f71e36882d7808547d0
Neto.Changes <infero-sky@yandex.ru>
2024-04-05 19:19:26 +0300

    Added HW Puppeteer 1

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -1,34 +1,71 @@
-let page;
-
-beforeEach(async () => {
-  page = await browser.newPage();
-  await page.goto("https://github.com/team");
-});
-
-afterEach(() => {
-  page.close();
-});
-
 describe("Github page tests", () => {
-  test("The h1 header content'", async () => {
-    const firstLink = await page.$("header div div a");
-    await firstLink.click();
-    await page.waitForSelector('h1');
-    const title2 = await page.title();
-    expect(title2).toEqual('GitHub: Where the world builds software · GitHub');
+  let page;
+
+  beforeAll(async () => {
+    page = await browser.newPage();
   });
 
-  test("The first link attribute", async () => {
-    const actual = await page.$eval("a", link => link.getAttribute('href') );
-    expect(actual).toEqual("#start-of-content");
+  afterAll(async () => {
+    await page.close();
   });
 
-  test("The page contains Sign in button", async () => {
-    const btnSelector = ".btn-large-mktg.btn-mktg";
-    await page.waitForSelector(btnSelector, {
-      visible: true,
+  describe("Tests on initial page", () => {
+    beforeEach(async () => {
+      await page.goto("https://github.com/team");
     });
-    const actual = await page.$eval(btnSelector, link => link.textContent);
-    expect(actual).toContain("Sign up for free")
+
+    test("The h1 header content'", async () => {
+      await page.setDefaultTimeout(50000);
+      const firstLink = await page.$("header div div a");
+      await firstLink.click();
+      await page.waitForSelector('h1');
+      const title2 = await page.title();
+      expect(title2).toEqual('GitHub for teams · Build like the best teams on the planet · GitHub');
+    });
+
+    test("The first link attribute", async () => {
+      await page.setDefaultTimeout(60000);
+      const actual = await page.$eval("a", link => link.getAttribute('href'));
+      expect(actual).toEqual("#start-of-content");
+    });
+
+    test("The page contains Sign in button", async () => {
+      await page.setDefaultTimeout(55000);
+      const btnSelector = ".btn-large-mktg.btn-mktg";
+      await page.waitForSelector(btnSelector, {
+        visible: true,
+      });
+      const actual = await page.$eval(btnSelector, link => link.textContent.trim());
+      expect(actual).toContain("Get started with Team");
+    });
+  });
+
+  describe("Tests on other pages", () => {
+    beforeEach(async () => {
+      await page.goto("https://github.com/features");
+    });
+
+    test("Header content on features page", async () => {
+      await page.setDefaultTimeout(50000);
+      await page.waitForSelector('h1');
+      const title = await page.title();
+      expect(title).toEqual('Features | GitHub · GitHub');
+    });
+
+    test("Header content on pricing page", async () => {
+      await page.setDefaultTimeout(55000);
+      await page.goto("https://github.com/pricing");
+      await page.waitForSelector('h1');
+      const title = await page.title();
+      expect(title).toEqual('Pricing · Plans for every developer · GitHub');
+    });
+
+    test("Header content on enterprise page", async () => {
+      await page.setDefaultTimeout(57000);
+      await page.goto("https://enterprise.github.com/");
+      await page.waitForSelector('h1');
+      const title = await page.title();
+      expect(title).toEqual('The AI Powered Developer Platform. · GitHub');
+    });
   });
 });
